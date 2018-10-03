97f6f642d8b8346535676a015ed4e6371488d858
Alexandria Walker-Wade <alexandriawalkerwade@gmail.com>
2018-10-03 23:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ if (name === "Susan") {
 }
 
 var height = "height"
-
+if ("height"" === "70") {
+    console.log("The height is correct")
+}
  
 // Don't worry about this
 module.exports = { name, height
